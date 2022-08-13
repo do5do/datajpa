f870c557feab4cb6fdfc5a10706198520f5de205
dodo <do151015@gmail.com>
2022-08-13 20:34:33 +0900

    0816

---
+++ src/main/java/entity/Team.java
@@ -0,0 +1,18 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}
